3b783a26a20efdeef51e79ebda607c2a26eeea92
liyulingyue <852433440@qq.com>
2024-09-28 10:44:41 +0800

    fix llm-functions

--- Tools/LLM/glm3.py
+++ Tools/LLM/glm3.py
@@ -147,3 +147,23 @@ class GLM3Class(object):
         print(partial_text)
         result = partial_text
         return result
+
+    def extract_json_from_llm_answer(self, result, start_str="```json", end_str="```", replace_list=["\n"]):
+        s_id = result.index(start_str)
+        e_id = result.index(end_str, s_id+len(start_str))
+        json_str = result[s_id+len(start_str):e_id]
+        for replace_str in replace_list:
+            json_str = json_str.replace(replace_str,"")
+        # print(json_str)
+        try:
+            json_dict = json.loads(json_str)
+        except Exception as e:
+            print("Error: ", e)
+            print("json_str: ", json_str)
+            json_dict = {}
+        return json_dict
+
+    def get_llm_json_answer(self, prompt):
+        result = self.get_llm_answer(prompt)
+        json_dict = self.extract_json_from_llm_answer(result)
+        return json_dict
--- Tools/LLM/gr_server.py
+++ Tools/LLM/gr_server.py
@@ -1,3 +1,5 @@
+import json
+
 from gradio_client import Client
 
 class GradioClass(object):
@@ -20,3 +22,23 @@ class GradioClass(object):
             api_name="/fn_get_response"
         )
         return result
+
+    def extract_json_from_llm_answer(self, result, start_str="```json", end_str="```", replace_list=["\n"]):
+        s_id = result.index(start_str)
+        e_id = result.index(end_str, s_id+len(start_str))
+        json_str = result[s_id+len(start_str):e_id]
+        for replace_str in replace_list:
+            json_str = json_str.replace(replace_str,"")
+        # print(json_str)
+        try:
+            json_dict = json.loads(json_str)
+        except Exception as e:
+            print("Error: ", e)
+            print("json_str: ", json_str)
+            json_dict = {}
+        return json_dict
+
+    def get_llm_json_answer(self, prompt):
+        result = self.get_llm_answer(prompt)
+        json_dict = self.extract_json_from_llm_answer(result)
+        return json_dict
